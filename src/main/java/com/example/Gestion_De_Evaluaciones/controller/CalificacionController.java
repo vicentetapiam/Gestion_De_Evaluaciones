638dcf7b158482d220e23655aabaa48e557cc440
vicho <vice.tapiam@duocuc.cl>
2025-05-28 12:21:58 -0400

    Delete calificacion

--- src/main/java/com/example/Gestion_De_Evaluaciones/controller/CalificacionController.java
+++ src/main/java/com/example/Gestion_De_Evaluaciones/controller/CalificacionController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -79,6 +80,19 @@ public class CalificacionController {
         }
     } 
 
+        @DeleteMapping("/{id}")
+    public ResponseEntity<Calificacion>deleteSistema(@PathVariable int id, Calificacion calificacion){
+        Calificacion buscado = calificacionService.findById(calificacion.getId());
+        if(buscado == null)
+        {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        else
+        {
+            calificacionService.delete(buscado);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+    }
 
     
 }
